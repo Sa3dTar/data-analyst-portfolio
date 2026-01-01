c415419733c37bef83d307258cc42d9b19346de5
Sa3dTar <164048550+Sa3dTar@users.noreply.github.com>
2026-01-01 18:51:59 +0200

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,9 @@
+const projects = [
+  {
+    title: "Sales Analysis",
+    description: "Project description here...",
+    github: "https://github.com/username/sales-analysis",
+    images: ["ecommerce_sales_analysis.png"], // الصور في root
+    excel: "Ecommerce Sales Analysis.xlsx - Data.xlsx" // ملف Excel في root
+  },
+];
